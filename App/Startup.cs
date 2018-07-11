723409bcaf6bffac4656476053ce7ae9da84051e
Chris <chris@Chrisde-MacBook-Pro.local>
2018-07-11 16:59:18 +0800

    調整
    Signed-off-by: Chris <chris@Chrisde-MacBook-Pro.local>

--- App/Startup.cs
+++ App/Startup.cs
@@ -15,6 +15,7 @@ namespace App
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
             services.AddSignalR()
             .AddMessagePackProtocol();
         }
@@ -28,6 +29,8 @@ namespace App
             }
 
             app.UseFileServer();
+
+            app.UseMvc();
 
             app.UseSignalR(routes =>
             {
